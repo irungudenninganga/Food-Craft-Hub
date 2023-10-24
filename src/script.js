54732d34ce80e1979d0df699159b2b9334418bea
+dennisngangairungu <+>
2023-10-24 19:39:28 +0300

    displaying content

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const mealDbURL='https://themealdb.com/api/json/v1/1/list.php?c=list'
+const mealDbURL='https://themealdb.com/api/json/v1/1/categories.php'
 const appendEl = document.getElementById('appendChild')
 let arrs={}
 
@@ -6,8 +6,23 @@ document.addEventListener('DOMContentLoaded', ()=> {
     fetch(mealDbURL)
     .then(res => res.json())
     .then(data => {
-       console.log(data)
-       
+        console.log(data)
+       console.log(data['categories'])
+
+        
+       for(arr of data['categories'] ){
+       console.log(arr)
+       appendEl.innerHTML+=`
+            <img src=${arr.strCategoryThumb}>
+            <h3 >${arr.strCategory}</h3>
+            <p > ${arr.strCategoryDescription}</p>
+
+       `
+
+        
+        
+
+        }
     })
 })
 
